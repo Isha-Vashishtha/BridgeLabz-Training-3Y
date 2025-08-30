99502d7a328126f5302572df7393cfa9a6d0a30e
Isha-Vashishtha <ishavashishtha862@gmail.com>
2025-08-30 21:42:14 +0530

    EXTRAS Java String Practice Problems

---
+++ EXTRAS-Java-String-Practice-Problems/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        String str = "programming";
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (result.indexOf(c) == -1) {
+                result += c;
+            }
+        }
+        System.out.println("String after removing duplicates: " + result);
+    }
+}
